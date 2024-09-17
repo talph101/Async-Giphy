e0744a407eac939e0cd96f675a856f11d1f719dc
Tiffany <139176913+talph101@users.noreply.github.com>
2024-09-17 12:27:47 -0500

    inserted query into endpoint url

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ require('dotenv').config();
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
 
-let endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=Web7hrcJnuWxC62zvBtx2WQkHFNDsNyC&q=25&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
-
 async function getImage(query) {
+    let endpoint = `https://api.giphy.com/v1/gifs/search?api_key=Web7hrcJnuWxC62zvBtx2WQkHFNDsNyC&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
     const response = await fetch (endpoint)
     const data = await response.json()
 }
 
-let url = "https://giphy.com/gifs/birthday-25-3oriNSevfPEs37Fex2"
+let url = "https://giphy.com/gifs/nehumanesociety-funny-dog-3o7527pa7qs9kCG78A"
 
 console.log(url);
