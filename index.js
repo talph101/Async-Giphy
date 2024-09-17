7c2b4681e0963f29cb6324859701fb74592f30b8
Tiffany <139176913+talph101@users.noreply.github.com>
2024-09-17 08:52:06 -0500

    endpoint for bday gif

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
 
+//endpoint for birthday gif
+
 let endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=Web7hrcJnuWxC62zvBtx2WQkHFNDsNyC&q=25&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
 
 async function getImage(query) {
